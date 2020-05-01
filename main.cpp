2dffecc849564155fa4924af2851874c5d7f2189
Václav Kubernát <sir.venceslas@gmail.com>
2020-05-01 13:20:32 +0200

    Change example program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <Arduino.h>
 #include "arduino_decls.hpp"
 
+namespace lib {
+class string {
+public:
+    string(size_t len)
+        : m_str(new char[len])
+    {
+    }
+
+    char* get()
+    {
+        return m_str;
+    }
+
+    ~string()
+    {
+        delete[] m_str;
+    }
+
+private:
+    char* m_str;
+};
+}
+
+long square(long x)
+{
+    return x * x;
+}
+
 int main(void)
 {
     init();
@@ -13,9 +41,12 @@ int main(void)
 
     Serial.begin(9600);
 
-    for (;;) {
-        Serial.write("AHOJDA");   // read it and send it out Serial1 (pins 0 & 1)
-        delay(1000);
+    lib::string toPrint(300);
+    long counter = 0;
+    while (true) {
+        sprintf(toPrint.get(), "%ld * %ld = %ld", counter, counter, square(counter));
+        Serial.println(toPrint.get());
+        counter++;
 
         if (serialEventRun) serialEventRun();
     }
